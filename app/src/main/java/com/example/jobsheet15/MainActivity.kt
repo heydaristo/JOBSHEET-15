95664fe2f70b36ca1a2074ffd4c7547e7b9b37d7
heydaristo <heydaristo@gmail.com>
2022-09-26 11:03:13 +0700

    JOBSHEET 15

---
+++ app/src/main/java/com/example/jobsheet15/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.jobsheet15
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnnama = findViewById<Button>(R.id.nama)
+        val btnkelas = findViewById<Button>(R.id.kelas)
+        val btnnis = findViewById<Button>(R.id.nis)
+
+        btnnama.setOnClickListener {
+            Toast.makeText(this, "Heydaristo Zhahir Razaq", Toast.LENGTH_SHORT).show()
+        }
+        btnkelas.setOnClickListener {
+            Toast.makeText(this, "XI PPLG 1", Toast.LENGTH_SHORT).show()
+        }
+        btnnis.setOnClickListener {
+            Toast.makeText(this, "2996", Toast.LENGTH_SHORT).show()
+        }
+
+    }
+}
